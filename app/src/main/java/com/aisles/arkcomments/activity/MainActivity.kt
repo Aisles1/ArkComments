72cfc144133edb70cbbd08946b0c7e0821de65ef
Aisles <531482780@qq.com>
2022-10-05 18:15:16 +0800

    添加viewBinding

--- app/src/main/java/com/aisles/arkcomments/activity/MainActivity.kt
+++ app/src/main/java/com/aisles/arkcomments/activity/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.aisles.arkcomments.activity
 
 import android.os.Bundle
-import com.aisles.arkcomments.R
 import com.aisles.arkcomments.base.BaseActivity
+import com.aisles.arkcomments.databinding.ActivityMainBinding
 
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         darkFont = true
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
     }
 
